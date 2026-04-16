1aa3d4839fff81d1f4a749fe362d9fd2c000ed63
Luccanjm <lucnjm@gmail.com>
2026-04-16 17:09:17 -0300

    Add django view hello world post

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def hello_post(request):
+    return HttpResponse("Hello World")
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import hello_post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello_post),
 ]
